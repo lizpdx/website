fd008878788110d733939f40a234e932a5f28a56
lizthedrummer <savagelist@mac.com>
2013-06-18 19:24:45 -0700

    Add blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '998a4e6f9c40d1f1b06cd9217e165831aa1006a14f7e4a621c96bfb7cdace6de6522dc7498c6a986576e86c941f82752337fbbfa78cc5dd672f85a89f906cbf8'
